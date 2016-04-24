536eaf86a7dab407ad258363939dae36f708dcfc
Jindrich B <jindra.basek@gmail.com>
2016-04-24 16:24:52 +0200

    ==11== Size optimizations

--- DHT.h
+++ DHT.h
@@ -1,17 +1,17 @@
 /* DHT library
 
-MIT license
-written by Adafruit Industries
-*/
+ MIT license
+ written by Adafruit Industries
+ */
 #ifndef DHT_H
 #define DHT_H
 
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
+#include <Arduino.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <Task.h>
 
+#define ENABLE_DHT_22_ONLY
 
 // Uncomment to enable printing out nice debug messages.
 //#define DHT_DEBUG
@@ -21,11 +21,11 @@ written by Adafruit Industries
 
 // Setup debug printing macros.
 #ifdef DHT_DEBUG
-  #define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
-  #define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
+#define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
+#define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
 #else
-  #define DEBUG_PRINT(...) {}
-  #define DEBUG_PRINTLN(...) {}
+#define DEBUG_PRINT(...) {}
+#define DEBUG_PRINTLN(...) {}
 #endif
 
 // Define types of sensors.
@@ -34,42 +34,46 @@ written by Adafruit Industries
 #define DHT21 21
 #define AM2301 21
 
+enum DhtReadState {
+    DHT_GOOD, DHT_ERROR
+};
 
 class DHT {
-  public:
-   DHT(uint8_t pin, uint8_t type, uint8_t count=6);
-   void begin(void);
-   float readTemperature(bool S=false, bool force=false);
-   float convertCtoF(float);
-   float convertFtoC(float);
-   float computeHeatIndex(float temperature, float percentHumidity, bool isFahrenheit=true);
-   float readHumidity(bool force=false);
-   boolean read(bool force=false);
+private:
+    uint8_t data[5];
+    uint8_t _pin;
+#ifndef ENABLE_DHT_22_ONLY
+    uint8_t _type;
+#endif
 
- private:
-  uint8_t data[5];
-  uint8_t _pin, _type;
-  #ifdef __AVR
+#ifdef __AVR
     // Use direct GPIO access on an 8-bit AVR so keep track of the port and bitmask
     // for the digital pin connected to the DHT.  Other platforms will use digitalRead.
     uint8_t _bit, _port;
-  #endif
-  uint32_t _lastreadtime, _maxcycles;
-  bool _lastresult;
+#endif
+    uint32_t _maxcycles;
 
-  uint32_t expectPulse(bool level);
+public:
+    DHT(uint8_t pin, uint8_t type = DHT22);
+
+    DhtReadState read();
+    float getTemperature();
+    float getHumidity();
+    float computeHeatIndex(float temperature, float percentHumidity);
+
+private:
+    uint32_t expectPulse(bool level);
 
 };
 
 class InterruptLock {
-  public:
-   InterruptLock() {
-    noInterrupts();
-   }
-   ~InterruptLock() {
-    interrupts();
-   }
-
+public:
+    InterruptLock() {
+        noInterrupts();
+    }
+    ~InterruptLock() {
+        interrupts();
+    }
 };
 
 #endif
